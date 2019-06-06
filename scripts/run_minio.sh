e3ed6b54a63f295eb94c9329e6dce13bebb96fe8
Hyoil LEE <onetop21@gmail.com>
2019-06-06 08:55:28 +0000

    Run minio server as daemon

--- scripts/run_minio.sh
+++ scripts/run_minio.sh
@@ -1,4 +1,4 @@
 MINIO_DATA=$HOME/.mlad/master/data
 MINIO_CONFIG=$HOME/.mlad/master/minio
 mkdir -p $MINIO_DATA $MINIO_CONFIG
-docker run -p 9000:9000 --name minio --rm -v $MINIO_DATA:/data -v $MINIO_CONFIG:/root/.minio minio/minio server /data
+docker run -p 9000:9000 --name minio -d -v $MINIO_DATA:/data -v $MINIO_CONFIG:/root/.minio minio/minio server /data
