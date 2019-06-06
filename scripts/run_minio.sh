e7d27214aec8cb7d738cd7fc28da898cc7ae6b67
Hyoil <onetop21@gmail.com>
2019-06-06 18:06:57 +0900

    Check docker-machine to run minio

--- scripts/run_minio.sh
+++ scripts/run_minio.sh
@@ -1,4 +1,23 @@
 MINIO_DATA=$HOME/.mlad/master/data
 MINIO_CONFIG=$HOME/.mlad/master/minio
+
+if [ -d /hosthome ]; then
+    # In docker-machine
+    if [ -z $1 ]; then
+        echo 'HOST USERNAME :' $1
+    else
+        echo 'Run script with username of host.'
+        echo "$$ bash $0 [USERNAME]"
+        exit 1
+    fi
+
+    if [ -d /hosthome/$1 ]; then
+        ln -s /hosthome/$1/.mlad $HOME/.mlad
+    else
+        echo "Cannot find username $1 in host."
+        exit 1
+    fi
+fi
+
 mkdir -p $MINIO_DATA $MINIO_CONFIG
 docker run -p 9000:9000 --name minio -d -v $MINIO_DATA:/data -v $MINIO_CONFIG:/root/.minio minio/minio server /data
