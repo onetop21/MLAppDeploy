553108ac4a60dfc074889db3dc4913c2223a4c78
Hyoil LEE <onetop21@gmail.com>
2019-06-06 17:30:53 +0000

    Update script to run private docker registry.

--- scripts/run_background_services.sh
+++ scripts/run_background_services.sh
@@ -1,5 +1,6 @@
-MINIO_DATA=$HOME/.mlad/master/data
-MINIO_CONFIG=$HOME/.mlad/master/minio
+# Check in docker-machine
+
+MLAD=$HOME/.mlad
 
 if [ -d /hosthome ]; then
     # In docker-machine
@@ -12,12 +13,20 @@ if [ -d /hosthome ]; then
     fi
 
     if [ -d /hosthome/$1 ]; then
-        ln -s /hosthome/$1/.mlad $HOME/.mlad
+        ln -s /hosthome/$1/.mlad $MLAD
     else
         echo "Cannot find username $1 in host."
         exit 1
     fi
 fi
+
+
+################
+# MinIO Server #
+################
+
+MINIO_DATA=$MLAD/master/data
+MINIO_CONFIG=$MLAD/master/minio
 
 mkdir -p $MINIO_DATA $MINIO_CONFIG
 
@@ -30,7 +39,7 @@ if [ -z $SECRET_KEY ]; then
 fi
 
 # Run minio server
-docker run -p 9000:9000 --name minio -d -e "MINIO_ACCESS_KEY=$ACCESS_KEY" -e "MINIO_SECRET_KEY=$SECRET_KEY" -v $MINIO_DATA:/data -v $MINIO_CONFIG:/root/.minio minio/minio server /data
+docker run -d -p 9000:9000 --restart=always --name minio -e "MINIO_ACCESS_KEY=$ACCESS_KEY" -e "MINIO_SECRET_KEY=$SECRET_KEY" -v $MINIO_DATA:/data -v $MINIO_CONFIG:/root/.minio minio/minio server /data
 
 # Run minio server by service(Swarm)
 #echo $ACCESS_KEY | docker secret create access_key -
@@ -38,8 +47,17 @@ docker run -p 9000:9000 --name minio -d -e "MINIO_ACCESS_KEY=$ACCESS_KEY" -e "MI
 #docker service create --name="minio" --secret="access_key" --secret="secret_key" -v $MINIO_DATA:/data -v $MINIO_CONFIG:/root/.minio minio/minio server /data
 
 
+####################
+# Docker Registery #
+####################
+
+REGISTRY_CONFIG=$MLAD/master/registry
+
+mkdir -p $REGISTRY_CONFIG
+
 # config.yml
-"version: 0.1
+cat > $REGISTRY_CONFIG/config.yml << EOL
+version: 0.1
 log:
     fields:
         service: registry
@@ -49,14 +67,15 @@ storage:
     cache:
         layerinfo: inmemory
     s3:
-        accesskey: {MINIO_ACCESS_KEY}
-        secretkey: {MINIO_SECRET_KEY}
-        region: {REGION}
-        regionendpoint: {REGION_ENDPOINT}
-        bucket: {BUCKET_NAME}
+        accesskey: $ACCESS_KEY
+        secretkey: $SECRET_KEY
+        region: "us-east-1"
+        regionendpoint: "http://localhost:9000"
+        bucket: "docker-registry"
         encrypt: false
         secure: true
         v4auth: true
         chunksize: 5242880
-        rootdirectory: /"
-docker run -d -p 5000:5000 --restart=always -v /mnt/registry:/var/lib/registry --name registry registry:2
+        rootdirectory: /
+EOL
+docker run -d -p 5000:5000 --restart=always -v $REGISTRY_CONFIG:/etc/docker/registry --name registry registry:2
