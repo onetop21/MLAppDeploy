d7f54c8b5df10a2974f7921258eedcf9adda8907
Hyoil <onetop21@gmail.com>
2019-06-06 17:46:54 +0900

    Update run_minio.sh script

--- scripts/run_minio.sh
+++ scripts/run_minio.sh
@@ -1,1 +1,4 @@
-docker run -p 9000:9000 -v /mnt/data:/data -v /mnt/config:/root/.minio minio/minio server /data
+MINIO_DATA=$HOME/.mlad/master/data
+MINIO_CONFIG=$HOME/.mlad/master/minio
+mkdir -p $MINIO_DATA $MINIO_CONFIG
+docker run -p 9000:9000 --name minio --rm -v $MINIO_DATA:/data -v $MINIO_CONFIG:/root/.minio minio/minio server /data
