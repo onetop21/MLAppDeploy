ffa744154147960052dbbbce50c6ff890ec0c600
Hyoil <onetop21@gmail.com>
2019-06-24 21:05:03 +0900

    Update register certificates code

--- scripts/register_cert.sh
+++ scripts/register_cert.sh
@@ -10,7 +10,7 @@ if [ ! -z $URL ]; then
     URL_NOARGS="${URL%%\?*}"
     DOMAIN=`echo $URL_NOARGS | awk -F/ '{print $6}'`
     sudo mkdir -p "/etc/docker/certs.d/${DOMAIN/-/:}"
-    sudo curl -o "/etc/docker/certs.d/${DOMAIN/-/:}/ca.crt" $URL
+    sudo curl -o "/etc/docker/certs.d/${DOMAIN/-/:}/ca.crt" "$URL"
     echo "Registered."
 else
     echo "No URL."
