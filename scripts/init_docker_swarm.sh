e66451968a7303582f27d62bc85fd63f2f18b2d2
hyoil LEE <onetop21@gmail.com>
2020-01-11 11:36:50 +0900

    Update Installation Script

--- scripts/init_docker_swarm.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-echo Clear Swarm Setting...
-docker swarm leave --force >> /dev/null 2>&1
-docker container prune -f >> /dev/null 2>&1
-docker network prune -f >> /dev/null 2>&1
-docker network create --subnet 10.10.0.0/24 --gateway 10.10.0.1 -o com.docker.network.bridge.enable_icc=false -o com.docker.network.bridge.name=docker_gwbridge docker_gwbridge
-docker swarm init $@
--- scripts/k8s/install_nvidia_docker_runtime.sh
+++ scripts/k8s/install_nvidia_docker_runtime.sh
@@ -1,1 +1,33 @@
-../install_nvidia_docker_runtime.sh
+# If you have nvidia-docker 1.0 installed: we need to remove it and all existing GPU containers
+docker volume ls -q -f driver=nvidia-docker | xargs -r -I{} -n1 docker ps -q -a -f volume={} | xargs -r docker rm -f
+sudo apt-get purge -y nvidia-docker
+
+# Add the package repositories
+curl -s -L https://nvidia.github.io/nvidia-docker/gpgkey | \
+  sudo apt-key add -
+distribution=$(. /etc/os-release;echo $ID$VERSION_ID)
+curl -s -L https://nvidia.github.io/nvidia-docker/$distribution/nvidia-docker.list | \
+  sudo tee /etc/apt/sources.list.d/nvidia-docker.list
+sudo apt-get update
+
+# Install nvidia-docker2 and reload the Docker daemon configuration
+sudo apt-get install -y nvidia-docker2 jq
+sudo pkill -SIGHUP dockerd
+
+# Test nvidia-smi with the latest official CUDA image
+docker run --runtime=nvidia --rm nvidia/cuda:9.0-base nvidia-smi
+
+# Enable GPU on docker swarm
+DAEMON_JSON="/etc/docker/daemon.json"
+if [ -z `sudo cat $DAEMON_JSON` ]; then
+    echo '{}' | sudo tee $DAEMON_JSON
+fi
+GPU_IDS=`nvidia-smi -a | grep UUID | awk '{print substr($4,0,12)}'`
+CONFIG=`sudo cat $DAEMON_JSON | jq '."default-runtime"="nvidia"' | jq '."node-generic-resources"=[]'`
+for ID in $GPU_IDS; do
+    CONFIG=`echo $CONFIG | jq '."node-generic-resources"=["gpu='$ID'"]+."node-generic-resources"'`
+done
+echo $CONFIG | jq . | sudo tee $DAEMON_JSON
+
+# Advertise GPU device to swarm.
+sudo sed -i -e 's/#swarm-resource/swarm-resource/' /etc/nvidia-container-runtime/config.toml
