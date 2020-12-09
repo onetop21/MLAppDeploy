9330c74ca8b4f135a5d6562b07d25179261a7a84
Hyoil LEE <onetop21@gmail.com>
2020-12-09 21:38:57 +0900

    Update install-mlad-env.sh

--- scripts/install-mlad-env.sh
+++ scripts/install-mlad-env.sh
@@ -176,8 +176,8 @@ function InstallNVIDIAContainerRuntime2008 {
 
     # Add the package repositories
     curl -s -L https://nvidia.github.io/nvidia-container-runtime/gpgkey | \
-      sudo apt-key add-
-    distribution=$(. /etc/os-release;echo $ID$VERSION)
+      sudo apt-key add -
+    distribution=$(. /etc/os-release;echo $ID$VERSION_ID)
     curl -s -L https://nvidia.github.io/nvidia-container-runtime/$distribution/nvidia-container-runtime.list | \
       sudo tee /etc/apt/sources.list.d/nvidia-container-runtime.list
     sudo apt-get update
