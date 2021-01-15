1d74a91719e40a443a57d06a90c104dcaa8d3499
Hyoil LEE <onetop21@gmail.com>
2021-01-15 16:34:59 +0900

    Add print admin token when starting app

--- frontend/mladservice/main.py
+++ frontend/mladservice/main.py
@@ -1,17 +1,29 @@
 from fastapi import FastAPI, Depends, Header
+from fastapi.logger import logger
 from mladservice.routers import image, service, project, node, auth
 from mladservice.auth import Authorization
+from mladcli.libs.auth import generate_admin_token
 
 def create_app():
     app = FastAPI()
+
+    user = Authorization('user')
+    admin = Authorization('admin')
+
+    app.include_router(image.router)
+    app.include_router(node.router, dependencies=[Depends(admin.verify_auth)])
+    app.include_router(service.router)
+    app.include_router(project.router, dependencies=[Depends(user.verify_auth)])
+    app.include_router(auth.router,dependencies=[Depends(admin.verify_auth)])
+        
+    print(f"Admin Token : {generate_admin_token().decode()}")
+
     return app
 
-user = Authorization('user')
-admin = Authorization('admin')
-
 app = create_app()
-app.include_router(image.router)
-app.include_router(node.router, dependencies=[Depends(admin.verify_auth)])
-app.include_router(service.router)
-app.include_router(project.router, dependencies=[Depends(user.verify_auth)])
-app.include_router(auth.router,dependencies=[Depends(admin.verify_auth)])
+if __name__ == '__main__':
+    '''
+    For Debugging
+    '''
+    import uvicorn
+    uvicorn.run(app, host='0.0.0.0')
