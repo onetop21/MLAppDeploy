8fcdea80e59d9640be779463de4bb7d739d7886e
Hyoil LEE <onetop21@gmail.com>
2021-02-09 03:26:32 +0900

    Modify service build script

--- temp.service/deploy.sh
+++ temp.service/deploy.sh
@@ -1,3 +1,13 @@
 #!/bin/bash
-docker build -t mlad/service -f MLADService-Dockerfile .
-docker service create --name mlad-service -p 8440:8440 --mount 'type=bind,src=/var/run/docker.sock,dst=/var/run/docker.sock' mlad/service
+
+if [[ "$1" == "build" ]]; then
+    #docker build -t mlad/service -f MLADService-Dockerfile .
+    docker build -t mlad/service -f MLADService-Dockerfile ..
+elif [[ "$1" == "up" ]]; then
+    docker service create --name mlad-service -p 8440:8440 --mount 'type=bind,src=/var/run/docker.sock,dst=/var/run/docker.sock' mlad/service
+elif [[ "$1" == "down" ]]; then
+    docker service rm mlad-service
+else
+    echo "MLAppDeploy Deployment Script"
+    echo "$ bash $0 [build,up,down]"
+fi
