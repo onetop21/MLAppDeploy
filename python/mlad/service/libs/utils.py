30d0657ff30c7a4984f628e1ae7b2e4c64afc452
kkkdeon <gad03149@naver.com>
2021-08-24 12:00:39 +0900

    Delete unused lines

--- python/mlad/service/libs/utils.py
+++ python/mlad/service/libs/utils.py
@@ -4,17 +4,6 @@ from mlad.core.default import config as service_config
 from mlad.cli import config as config_core
 
 
-CONFIG_FILE = os.environ.get('MLAD_CONFIG_PATH', '/config.yaml')
-
-
-def read_config():
-    try:
-        config = config_core.get()
-    except Exception:
-        config = {}
-    return config
-
-
 def is_debug_mode():
     if '-d' in sys.argv or '--debug' in sys.argv or os.environ.get(
             'MLAD_DEBUG', service_config['mlad']['debug']):
