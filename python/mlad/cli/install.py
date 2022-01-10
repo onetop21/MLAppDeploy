5b9d532d47dc33fcb0e2936f44a531b66f903fd2
JunhoeKim <jooon.hoe94@gmail.com>
2022-01-10 11:24:08 +0900

    feat: mlad install api-server command 구현 (#243)
    * feat: deploy_api_server 안에서 secret을 생성하도록 구현
    
    * fix: argument에서 help 메세지 제거
    
    * fix: typo
    
    * fix: secret not found error 처리
    
    * feat: 나머지 resources 생성 로직 추가
    
    * feat: ingress 옵션 처리
    
    * fix: 안내 메세지 변경
    
    * fix: merge가 제대로 안된 부분 수정
    
    * fix: delete installer.py
    
    * fix: mlad-service를 patch하는 방식 변경

--- python/mlad/cli/install.py
+++ python/mlad/cli/install.py
@@ -1,4 +1,4 @@
-from mlad.core import exceptions as core_exception
+from mlad.core import exceptions as core_exceptions
 from mlad.core.kubernetes import controller as ctlr
 from mlad.cli.libs import utils
 
@@ -6,7 +6,7 @@ from mlad.cli.libs import utils
 def has_kubeconfig() -> bool:
     try:
         ctlr.get_current_context()
-    except core_exception.APIError:
+    except core_exceptions.APIError:
         return False
     return True
 
@@ -45,7 +45,7 @@ def check():
     # Check ingress controller
     try:
         res = ctlr.check_ingress(cli, 'dummy-ingress', 'mlad')
-    except core_exception.NotFound:
+    except core_exceptions.NotFound:
         ctlr.create_ingress(cli, 'mlad', 'mlad-service', 'dummy-ingress', 8440, '/dummy')
         res = ctlr.check_ingress(cli, 'dummy-ingress', 'mlad')
     if res:
@@ -55,7 +55,7 @@ def check():
     # Check metrics server
     try:
         ctlr.get_deployment(cli, 'metrics-server', 'kube-system')
-    except core_exception.NotFound:
+    except core_exceptions.NotFound:
         pass
     else:
         checked['Metrics Server']['status'] = True
@@ -63,7 +63,7 @@ def check():
     # Check nvidia device plugin
     try:
         ctlr.get_daemonset(cli, 'nvidia-device-plugin-daemonset', 'kube-system')
-    except core_exception.NotFound:
+    except core_exceptions.NotFound:
         pass
     else:
         checked['NVIDIA Device Plugin']['status'] = True
@@ -71,11 +71,11 @@ def check():
     # Check node feature discovery
     try:
         ctlr.get_daemonset(cli, 'nfd', 'node-feature-discovery')
-    except core_exception.NotFound:
-        checked['Node Feature Discovery']['msgs'].append('"nfd" not found. Run \'kubectl apply -f https://raw.githubusercontent.com/NVIDIA/gpu-feature-discovery/v0.4.1/deployments/static/nfd.yaml\'.')
+    except core_exceptions.NotFound:
+        checked['Node Feature Discovery']['msgs'].append('"nfs" not found. Run \'kubectl apply -f https://raw.githubusercontent.com/NVIDIA/gpu-feature-discovery/v0.4.1/deployments/static/nfd.yaml\'.')
     try:
         ctlr.get_daemonset(cli, 'gpu-feature-discovery', 'node-feature-discovery')
-    except core_exception.NotFound:
+    except core_exceptions.NotFound:
         checked['Node Feature Discovery']['msgs'].append('"gpu-feature-discovery" not found. Run \'kubectl apply -f https://raw.githubusercontent.com/NVIDIA/gpu-feature-discovery/v0.4.1/deployments/static/gpu-feature-discovery-daemonset.yaml -n node-feature-discovery\'.')
     finally:
         if len(checked['Node Feature Discovery']['msgs']) == 0:
@@ -86,7 +86,7 @@ def check():
     # Check mlad api server
     try:
         ctlr.get_deployment(cli, 'mlad-service', 'mlad')
-    except core_exception.NotFound:
+    except core_exceptions.NotFound:
         pass
     else:
         checked['MLAD API Server']['status'] = True
@@ -101,4 +101,53 @@ def check():
 
         if not status:
             for line in msgs:
-                yield f'· {line}'
+                yield f'˙ {line}'
+
+
+def _is_running_api_server(cli) -> bool:
+    try:
+        ctlr.get_deployment(cli, 'mlad-api-server', 'mlad')
+    except core_exceptions.NotFound:
+        return False
+
+    return True
+
+
+def deploy_api_server(image_tag: str, ingress: bool):
+    cli = ctlr.get_api_client(context=ctlr.get_current_context())
+    is_running = _is_running_api_server(cli)
+    if not is_running:
+        yield 'Create docker registry secret named \'docker-mlad-sc\'.'
+        ctlr.create_docker_registry_secret(cli)
+
+        yield 'Create \'mlad\' namespace.'
+        ctlr.create_mlad_namespace(cli)
+
+        yield 'Create \'mlad-cluster-role\' cluster role.'
+        yield 'Create \'mlad-cluster-role-binding\' cluster role binding.'
+        ctlr.create_api_server_role_and_rolebinding(cli)
+
+        yield 'Create \'mlad-service\' service.'
+        ctlr.create_mlad_service(cli, nodeport=not ingress)
+        if not ingress:
+            yield 'Check the node port value of the \'mlad-service\'.'
+            yield 'Run \'kubectl get svc -n mlad\'.'
+
+        yield 'Create \'mlad-api-server\' deployment.'
+        ctlr.create_mlad_api_server_deployment(cli, image_tag)
+    else:
+        yield 'Patch the \'mlad-service\' service.'
+        ctlr.patch_mlad_service(cli, nodeport=not ingress)
+        if not ingress:
+            yield 'Check the NodePort value of the \'mlad-service\'.'
+            yield 'Run \'kubectl get svc -n mlad\'.'
+
+        yield 'Patch the \'mlad-api-service\' deployment.'
+        ctlr.patch_mlad_api_server_deployment(cli)
+
+    if ingress:
+        yield 'Create \'mlad-ingress\' ingress.'
+        ctlr.create_mlad_ingress(cli)
+    else:
+        yield 'Delete running \'mlad-ingress\' ingress.'
+        ctlr.delete_mlad_ingress(cli)
--- python/mlad/cli/install_cli.py
+++ python/mlad/cli/install_cli.py
@@ -11,9 +11,20 @@ def check():
         click.echo(line)
 
 
+@click.command('api-server')
+@click.argument('image-tag', required=True)
+@click.option('--ingress', is_flag=True, help='Use an ingress to expose the api server.')
+@echo_exception
+def api_server(image_tag: str, ingress: bool):
+    '''Deploy the MLAD api server.'''
+    for line in install.deploy_api_server(image_tag, ingress):
+        click.echo(line)
+
+
 @click.group('install')
 def cli():
     '''Manage installation.'''
 
 
 cli.add_command(check)
+cli.add_command(api_server)
