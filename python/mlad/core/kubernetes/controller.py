0b4321c42ce54e626d6251453ff2ea90a1257dcc
kkkdeon <gad03149@naver.com>
2022-02-11 10:06:49 +0000

    fix: default값 수정

--- python/mlad/core/kubernetes/controller.py
+++ python/mlad/core/kubernetes/controller.py
@@ -515,7 +515,7 @@ def inspect_app(app, cli=DEFAULT_CLI):
         'replicas': app.spec.parallelism if kind == 'Job' else app.spec.replicas,
         'tasks': dict([(pod.metadata.name, get_pod_info(pod)) for pod in pod_ret.items]),
         'ports': [port_spec.port for port_spec in service.spec.ports] if service is not None else [],
-        'ingress': json.loads(config_labels.get('MLAD.PROJECT.INGRESS', '')),
+        'ingress': json.loads(config_labels.get('MLAD.PROJECT.INGRESS', '[]')),
         'created': app.metadata.creation_timestamp,
         'kind': config_labels.get('MLAD.PROJECT.APP.KIND'),
     }
