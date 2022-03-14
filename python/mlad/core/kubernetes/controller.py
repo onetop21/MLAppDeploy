14a2fe36ac29092d25251bb4bb132166f394cc9a
JunhoeKim <jooon.hoe94@gmail.com>
2022-03-14 15:46:47 +0900

    refactor: inspect_app의 return 값이 expose가 되도록 변경 (#363)
    * refactor: inspect_app의 return 값이 expose가 되도록 변경
    
    * fix: values() type을 list로 변경
    
    * fix: 안쓰는 ingress 관련 코드 삭제

--- python/mlad/core/kubernetes/controller.py
+++ python/mlad/core/kubernetes/controller.py
@@ -420,13 +420,6 @@ def inspect_app(app: App, cli: ApiClient = DEFAULT_CLI) -> Dict:
     hostname, path = config_labels.get('MLAD.PROJECT.WORKSPACE', ':').split(':')
     pod_spec = app.spec.template.spec
     service = get_k8s_service_of_app(namespace, name, cli=cli)
-    try:
-        ingress = json.loads(config_labels.get('MLAD.PROJECT.INGRESS', '[]'))
-    except json.decoder.JSONDecodeError:
-        path = config_labels.get('MLAD.PROJECT.INGRESS', '')
-        ingress = []
-        if path != '':
-            ingress.append({'port': '-', 'path': path})
 
     spec = {
         'key': config_labels['MLAD.PROJECT'] if config_labels.get(
@@ -449,12 +442,26 @@ def inspect_app(app: App, cli: ApiClient = DEFAULT_CLI) -> Dict:
         'name': config_labels.get('MLAD.PROJECT.APP'),
         'replicas': app.spec.parallelism if kind == 'Job' else app.spec.replicas,
         'tasks': dict([(pod.metadata.name, get_pod_info(pod)) for pod in pods]),
-        'ports': [port_spec.port for port_spec in service.spec.ports] if service is not None else [],
-        'ingress': ingress,
+        'expose': _obtain_app_expose(service, config_labels),
         'created': app.metadata.creation_timestamp,
         'kind': config_labels.get('MLAD.PROJECT.APP.KIND'),
     }
     return spec
+
+
+def _obtain_app_expose(service: Optional[client.V1Service], config_labels: Dict[str, str]) -> List[Dict]:
+    if service is None:
+        return []
+    expose_dict = {str(spec.port): {'port': spec.port} for spec in service.spec.ports}
+    try:
+        ingresses = json.loads(config_labels.get('MLAD.PROJECT.INGRESS', '[]'))
+    except json.decoder.JSONDecodeError:
+        ingresses = []
+    for ingress in ingresses:
+        ingress_port = str(ingress['port'])
+        if ingress_port in expose_dict:
+            expose_dict[ingress_port]['ingress'] = {'path': ingress['path']}
+    return list(expose_dict.values())
 
 
 def inspect_apps(apps: List[App]) -> List[Dict]:
