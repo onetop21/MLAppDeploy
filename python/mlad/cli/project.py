7009ab1737b4048070dae71d53318c40fcd72fcd
JunhoeKim <jooon.hoe94@gmail.com>
2022-03-30 18:26:32 +0900

    fix: image tag가 달라지면 달라진 이미지로 업데이트하도록 변경 (#394)
    * fix: default_update_spec에 project image를 넣도록 변경
    
    * fix: image tag가 달라지면 변경 메세지 출력하도록 적용
    
    * fix: update spec에 추가되는 조건 추가
    
    * fix: 주석 추가 및 이미지 태그 변경 안내문 수정
    
    * fix: update timeout 시간 조정

--- python/mlad/cli/project.py
+++ python/mlad/cli/project.py
@@ -479,11 +479,12 @@ def scale(file: Optional[str], project_key: Optional[str], scales: List[Tuple[st
 
 def update(file: Optional[str], project_key: Optional[str]):
     utils.process_file(file)
+    config = config_core.get()
     if project_key is None:
         project_key = utils.workspace_key()
     project = API.project.inspect(project_key=project_key)
     cur_project_yaml = json.loads(project['project_yaml'])
-    image_tag = project['image']
+    cur_image_tag = project['image']
 
     project = utils.get_project()
 
@@ -491,8 +492,19 @@ def update(file: Optional[str], project_key: Optional[str]):
     if not kind == 'Deployment':
         raise InvalidProjectKindError('Deployment', 'deploy')
 
+    base_labels = utils.base_labels(
+        utils.get_workspace(),
+        config['session'],
+        project,
+        config_core.get_registry_address(config)
+    )
+    image_tag = base_labels[MLAD_PROJECT_IMAGE]
+    if cur_image_tag != image_tag:
+        yield f'Image tag [{cur_image_tag}] and [{image_tag}] are different.'
+        yield f'The base image will be updated to [{image_tag}].'
+
     default_update_spec = {
-        'image': None,
+        'image': image_tag,
         'command': None,
         'args': None,
         'scale': 1,
@@ -567,7 +579,8 @@ def update(file: Optional[str], project_key: Optional[str]):
             yield utils.info_msg(f"Warning: '{name}' env {env_ignored} "
                                  'will be ignored for MLAD preferences.')
 
-        if len(diff_keys[name]) > 0:
+        # Add an update spec if there are any changes in the app spec or image tag
+        if len(diff_keys[name]) > 0 or image_tag != cur_image_tag:
             update_specs.append(update_spec)
 
     for name, keys in diff_keys.items():
